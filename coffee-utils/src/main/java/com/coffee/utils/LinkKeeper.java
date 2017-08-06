8a460e8e7b206ffaffd4e37ae23ef8ea6abc2199
denis.tmenov@gmail.com <myGITHUB2788>
2017-08-06 12:07:04 +0300

    corrected utils

---
+++ coffee-utils/src/main/java/com/coffee/utils/LinkKeeper.java
@@ -0,0 +1,18 @@
+package com.coffee.utils;
+
+public final class LinkKeeper {
+	public static final String PAGE_COFFEE_LIST = "CoffeeList";
+	public static final String PAGE_ORDER_LIST = "OrderList";
+	public static final String PAGE_ORDER = "Order";
+	public static final String PAGE_ERROR404 = "Error404";
+
+	public static final String JSP_HEADER = "/WEB-INF/pages/frame/page/header.jsp";
+	public static final String JSP_MENU = "/WEB-INF/pages/frame/page/menu.jsp";
+	public static final String JSP_FOOTER = "/WEB-INF/pages/frame/page/footer.jsp";
+
+	public static final String JSP_COFFEE_LIST = "/WEB-INF/pages/coffeeList.jsp";
+	public static final String JSP_ORDER_LIST = "/WEB-INF/pages/orderList.jsp";
+	public static final String JSP_ORDER = "/WEB-INF/pages/order.jsp";
+	public static final String JSP_ERROR404 = "/WEB-INF/pages/errorPage404.jsp";
+
+}

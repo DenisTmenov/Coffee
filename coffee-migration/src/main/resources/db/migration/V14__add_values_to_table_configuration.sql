2cf5a408b25b25e89a601d79c07b290cbf5a61f6
DenisTmenov <denis.tmenov@gmail.com>
2017-08-14 15:19:58 +0300

    new migrations

---
+++ coffee-migration/src/main/resources/db/migration/V14__add_values_to_table_configuration.sql
@@ -0,0 +1,3 @@
+INSERT INTO configuration (`id`, `name`, `value`, `transcript`) VALUES (1, "n", 5, "Count of identical cups of coffee.");
+INSERT INTO configuration (`id`, `name`, `value`, `transcript`) VALUES (2, "x", 100, "Minimal cost without shipping cost.");
+INSERT INTO configuration (`id`, `name`, `value`, `transcript`) VALUES (3, "m", 32, "Cost shipping.");
